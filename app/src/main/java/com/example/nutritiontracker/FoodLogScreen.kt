1ab288e9762c5b6e5a58b496e1ddd9cbe9d8edbb
KristofferNoScope <kkandelid@gmail.com>
2026-09-11 01:15:08 +0200

    Added NutrientRings, StepProgressBar to HomeScreen.

---
+++ app/src/main/java/com/example/nutritiontracker/FoodLogScreen.kt
@@ -0,0 +1,20 @@
+package com.example.nutritiontracker
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FoodLogScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Food Log Screen")
+    }
+}
